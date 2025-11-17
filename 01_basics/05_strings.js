e6b4e7876d6850e307121f034fcb46be05b50789
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-17 06:50:23 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,24 @@
+const name="abrar"
+const repCount=50
+// console.log(name+repCount)
+console.log(`${name} repeated ${repCount} times`)
+const gameName=new String("hitesh-hc")//creates object of string type
+// console.log(typeof gameName)
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(0))
+console.log(gameName.indexOf('m'))
+const newString=gameName.substring(0,4)
+console.log(newString)
+console.log(gameName.slice(-8,4))
+//chess
+
+console.log("      abrar    ".trim())
+console.log("      abrar    ")
+
+const url="https://www.youtube.com/watch?v=abcdefg"
+console.log(url.replace("youtube","google"))
+console.log(url.includes("youtube"))
+console.log(gameName.split('-'))//gives array of strings
