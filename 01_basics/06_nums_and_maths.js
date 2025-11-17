5d35f2b99bd3debd3c3c5aabf0726506d22dd39b
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-17 07:08:49 +0000

    maths and numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+// const score=400
+// console.log(score)
+
+
+// const balance=new Number(1000)
+// console.log(balance)
+// console.log(balance.toFixed(2).length)
+// console.log(balance.toString())
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
+
+// const otherNumber=123.123456
+// console.log(otherNumber.toPrecision(4))
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//Maths Library
+
+// console.log(Math.abs(-100))
+// console.log(Math.round(4.49))
+// console.log(Math.ceil(4.01))
+// console.log(Math.floor(4.99))
+// console.log(Math.min(0,150,30,20,-8,-200))
+// console.log(Math.max(0,150,30,20,-8,-200))
+console.log(Math.floor((Math.random()*10)+1))//gives random number between 0 and 1
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)//gives random number between min and max including both
