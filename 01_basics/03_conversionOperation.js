0b5a7f14e464f4644d59dd74f99e189c2a576efe
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-17 04:00:06 +0000

    more operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,43 @@
-let score=undefined
-console.log(typeof score)
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let score=undefined
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-//"33"=> 33
+// //"33"=> 33
 
-//"abrar" => NaN
-//"33abc" => NaN
-//null => 0
-//undefined =? NaN
-let isLoggedIn=""
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-// 1 => true, 0=> false
-// ""=> false, "abrar"=> true
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// //"abrar" => NaN
+// //"33abc" => NaN
+// //null => 0
+// //undefined =? NaN
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// // 1 => true, 0=> false
+// // ""=> false, "abrar"=> true
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+let value=3
+let negvalue=-value
+// console.log(negvalue)
+// console.log(2**2)
+// console.log(2%2)
+let str1="hello "
+let str2="abrar"
+let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+'2')
+// console.log(+true)
+// console.log(+"")
+// console.log(+"abrar")
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=1
+console.log(++gameCounter)
