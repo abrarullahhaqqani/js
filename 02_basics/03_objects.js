1452c2bb13c8a2c6dcf8eb975cb0936cf70b168f
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-17 15:16:31 +0000

    objects part 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,1 +1,29 @@
 //singleton
+//Object.create
+//object literals
+const mySym=Symbol("key1")
+const JsUser={
+    name:"Abrar",
+    [mySym]:"mykey1",
+    age:20,
+    Location:"Hyderabad",
+    email:"abrarullah.swe@gmail.com",
+    lastLogin:["Sunday","Friday"]
+}
+
+// console.log(JsUser.Location)
+// console.log(JsUser["lastLogin"])
+// console.log(JsUser[mySym]) //how symbols are printed
+// JsUser.email="abrarullah.haqqani@okta.com"
+// Object.freeze(JsUser)
+// JsUser.email="abrarullah.haqqani@servicenow.com"
+// console.log(JsUser)
+
+JsUser.greeting=function(){
+    console.log("Assalamualaikum")
+}
+JsUser.greetingtwo=function(){
+    console.log(`Assalamualaikum ${this.name}, Are you from ${this.Location}?`)
+}
+console.log(JsUser.greeting())
+console.log(JsUser.greetingtwo());
