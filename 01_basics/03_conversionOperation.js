06ae8b3ad4fbf1b8e27c5cc77e881149597c0e16
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-17 03:49:18 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score=undefined
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=> 33
+
+//"abrar" => NaN
+//"33abc" => NaN
+//null => 0
+//undefined =? NaN
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 => true, 0=> false
+// ""=> false, "abrar"=> true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
