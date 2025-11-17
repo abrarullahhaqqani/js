b31cc6b5754c04dca51a57bd7f7895fdeaf03a42
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-17 02:01:43 +0000

    datatypes explored

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//all code treated as new version code
+// alert(2+3)// we are using nodejs not browser
+console.log(3+
+    3)//Ensure code readability , this is not readable
+
+console.log("Abrarullah")
+
+let nme="Abrarullah"
+let age=20
+let isLoggedIn=false
+
+//number
+//bigint
+//boolean
+//string => ""
+//null => standalone value
+//undefined => no value assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof nme)
+console.log(typeof age)
+console.log(typeof null)//this is a object
+console.log(typeof undefined)//this is a undefined type
+
+
