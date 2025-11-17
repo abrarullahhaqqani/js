18c21b93abd19f81a6dc26e84a190f0e321bd918
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-17 04:08:26 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2!=1)
+
+// console.log("2">1)
+// console.log("002">1)
+
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(null<=0)
+// console.log(null==0)
+//Comparisions >=, <= convert null to 0
+// Whereas == does not convert null to 0
+
+// console.log(undefined==0)
+// console.log(undefined>=0)
+// console.log(undefined<=0) //Undefined gives false always
+
+console.log('0002'===2)//This is strict check it checks value and also the datatype
+
