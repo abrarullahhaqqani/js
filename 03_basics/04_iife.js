21795c533dbc6c827e2ddade50cbe368920c882e
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-18 11:33:05 +0000

    iife functions

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,4 +1,5 @@
 //Immediately Invoked Function Expressions (IIFE)
+//used in order to make sure the globa scope pollution does not happen
 (function chai(){
     //named IIFE
     console.log("Chai connected")
