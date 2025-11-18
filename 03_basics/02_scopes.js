2cc5cd327c77b5a6474965789d9278bb5f6afa7c
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-18 04:12:40 +0000

    functions and scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,39 @@
+// // let a=300
+// // if(true){
+// // let a=10
+// // const b=20
+// // // var c=30
+
+// // }
+// // console.log(a)
+// // console.log(b)
+// // console.log(c)
+
+// function one(){
+//     const username="abrar"
+//     function two(){
+//         const website="portfolio"
+//         console.log(username)
+//     }
+//     // console.log(website)
+//     two()
+// }
+
+// // one()
+// if(true){
+//     const username="abrar"
+//     if(username==="abrar"){
+//         const website="portfolio"
+//         console.log(username+website)
+//     }
+//     // console.log(website)
+// }
+// // console.log(username)
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+console.log(addtwo(2))
+const addtwo=function(num){
+    return num+2
+}
