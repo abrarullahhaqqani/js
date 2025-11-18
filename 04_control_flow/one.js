e305e6f243f8d1c8c8f79e512bb74edd9a70875d
abrarullahhaqqani <129473479+abrarullahhaqqani@users.noreply.github.com>
2025-11-18 15:08:41 +0000

    dom

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,3 +1,12 @@
-if(2=="-2"){
-    console.log("executed")
+const c=1;
+switch(c){
+    case 1:
+        console.log("hi")
+    break
+    case 2:
+        console.log("hello")
+    break
+    default:
+        console.log("bye")
+    break
 }
